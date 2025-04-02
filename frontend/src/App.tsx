d242d0ef1f822fe9d27f552c940b2673b56a0b9e
Siddhartha Sahu <siddharthasahu.work@gmail.com>
2025-04-03 03:02:22 +0530

    feat: routing setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+const Blog = lazy(() => import("./pages/Blog"));
+const Signin = lazy(() => import("./pages/Signin"));
+const Signup = lazy(() => import("./pages/Signup"));
 
 function App() {
   
-
   return (
     <>
-      <h1 className="">Hello World!</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
---
+++ frontend/src/pages/Blog.tsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+const Blog = () => {
+    const { id } = useParams<{ id: string }>();
+  return (
+    <div>Blog: {id}</div>
+  )
+}
+
+export default Blog;
